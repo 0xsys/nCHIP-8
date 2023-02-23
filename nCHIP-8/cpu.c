b38ee9c2598a79dc3960742d3ceb0b55fe45d241
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2023-02-23 21:47:03 +0800

    Created struct containing all components of the CHIP-8 system. Also implemented and initialisation function to provide memory for all required pointers within the aforementioned struct.

--- nCHIP-8/cpu.c
+++ nCHIP-8/cpu.c
@@ -1,5 +1,19 @@
-// Initialise memory for components of CHIP-8
+#include "cpu.h"
 
+// Initialise memory for components of CHIP-8
+void cpuInitialise(Components *chip8) {
+
+    // Chose calloc for memory allocation, as sets to 0 instead of garbage value.
+    
+    chip8->memory = calloc(4096, sizeof(uint8_t));
+
+    chip8->stack = calloc(16, sizeof(uint16_t));
+
+    chip8->V = calloc(16, sizeof(uint8_t));
+
+    chip8->PC = 0x200;
+
+}
 // Fetch decode execute
 
 // Free components resources
