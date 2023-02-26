ec903f6687a1c09494153aed10b01d37c0d9da90
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2023-02-26 10:52:09 +0800

    Implemented the necessary opcodes except for DXYN (display/draw). Also created the executeOpcode function.

--- nCHIP-8/opcodes.c
+++ nCHIP-8/opcodes.c
@@ -33,6 +33,10 @@ Instructions loadInstructionStruct(uint16_t opcode) {
     
     instruction.Y = (opcode & 0x00F0) >> 4;
 
+    instruction.NNN = opcode & 0x0FFF;
+
+    instruction.NN = opcode & 0x00FF;
+
 }
 
 // Opcode function definitions
@@ -46,5 +50,68 @@ void opcode0x00E0(Components *chip8) {
 
 }
 
-// Pass opcode details to function, execute match from list of opcodes
+// Jump to instruction NNN
+void opcode0x1NNN(Components *chip8, Instructions instruction) {
 
+    chip8->PC = instruction.NNN;
+
+}
+
+void opcode0x6XNN(Components *chip8, Instructions instruction) {
+
+    chip8->V[instruction.X] = instruction.NN;
+    
+    incrementPC(chip8);
+
+}
+
+void opcode0x7XNN(Components *chip8, Instructions instruction) {
+
+    state->v[instruction.X] += instruction.KK;
+
+    incrementPC(chip8);
+
+}
+
+void opcodeANNN(Components *chip8, Instructions instruction) {
+
+    chip8->I = instruction.NNN;
+
+    incrementPC(chip8);
+
+}
+
+// Pass opcode details to function, execute match from list of opcodes
+void executeOpcode(Components *chip8, Instructions instruction) {
+    switch(instruction.I) {
+        
+        case 0x0:
+            switch (instruction.full_opcode) {
+            case 0x00E0:
+                
+                opcode0x00E0(chip8);
+                break;
+
+            }
+    
+        case 0x1: 
+            opcode0x1NNN(chip8, instruction);
+            break;
+    
+        case 0x6:
+            opcode0x6XNN(chip8, instruction);
+            break;
+
+        case 0x7:
+            opcode0x7XNN(chip8, instruction);
+            break;
+
+        case 0xA:
+            opcodeANNN(chip8, instruction);
+
+        case default:
+            printf("Opcode unrecognised: %x\n", instruction.opcode);
+            exit(1);
+
+    }
+}
--- nCHIP-8/opcodes.h
+++ nCHIP-8/opcodes.h
@@ -10,11 +10,9 @@ typedef struct {
                //                   |-- Used to look up one of the 16 registers (VX & VY) from V0 - VF.
     uint8_t Y; // Third nibble   }--|
     
-    uint8_t J;
-    
     uint16_t NNN;
     
-    uint8_t KK; 
+    uint8_t NN; 
 
 } Instructions;
 
@@ -28,3 +26,13 @@ Instructions loadInstructionStruct(uint16_t opcode);
 // Opcode Instructions;
 
 void opcode0x00E0(Components *chip8);
+
+void opcode0x1NNN(Components *chip8, Instructions instruction);
+
+void opcode0x6XNN(Components *chip8, Instructions instruction);
+
+void opcode0x7XNN(Components *chip8, Instructions instruction);
+
+void opcodeANNN(Components *chip8, Instructions instruction);
+
+void executeOpcode(Components *chip8, Instructions instruction);
